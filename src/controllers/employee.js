2e0261226d4612fe4512cd7b53048ceafc2ba749
habaumugisha1 <habajeunes2.com>
2021-10-17 18:08:16 +0200

    updateand get all employees

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -1,5 +1,5 @@
 import models from '../models';
-import {employeeExist,employeeIdExist} from "../helpers"
+import {employeeExist,employeeIdExist, findEmployeeById} from "../helpers"
 import {sendVerificationEmail} from "../helpers"
 
 export const createNewEmployee = async (req, res) => {
@@ -10,7 +10,7 @@ const code = `EMP${randomNumber}`
 try{
    const employeeIsExist = await employeeExist(email);
    const nationalIdExist = await employeeIdExist(national_id)
-   
+
    if(employeeIsExist) return res.status(409).json({message:"Employee with the same email already exist"});
 
    if(nationalIdExist) return res.status(409).json({message:"national ID already exist"});
@@ -21,4 +21,18 @@ try{
  } catch (error) {
    return res.status(400).json({status:400, message: error.message});
  }
+}
+
+export const updateRecord = async (req,res) => {
+    const {employeeId} = req.params
+    const employee = findEmployeeById(employeeId)
+    if(!employee) return res.status(404).json({status:404, message:"Employee does not exist"});
+    await models.Employees.update(req.body, {where: {id: employeeId}})
+    return res.status(200).json({status:200, message:"Employee updated successful!"})
+}
+
+export const getAllRecord = async (req,res) => {
+
+    const allEmployees = await models.Employees.findAll()
+    return res.status(200).json({status:200, message:"Employee retrieved successful!", allEmployees})
 }
